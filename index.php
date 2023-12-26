4c2b113ce80802005cd65a4d281d6e8b9b8a4651
hellerqr <Tom-Jennes@web.de>
2023-12-26 12:25:21 +0100

    adding tabulators and remove an empty line

--- index.php
+++ index.php
@@ -6,21 +6,20 @@
     <body>
         <header>
             <div class="navbar">
-            <div class="logo">
-            <a href="index.php"><img src="images/logo.png" alt="Logo" 
-            style="width:42px;height:42px;"></a>
-            </div>
-            <div class="nav-items">
-                <li>
-                    <a id="nav-item" href="/index.php">Home</a>
-                </li>
-                <li>
-                    <a id="nav-item" href="/">Placeholder</a>
-                </li>
-                <li>
-                    <a id="nav-item" href="/">Placeholder</a>
-                </li>
-            </div>
+                <div class="logo">
+                    <a href="index.php"><img src="images/logo.png" alt="Logo" style="width:42px;height:42px;"></a>
+                </div>
+                <div class="nav-items">
+                    <li>
+                        <a id="nav-item" href="/index.php">Home</a>
+                    </li>
+                    <li>
+                        <a id="nav-item" href="/">Placeholder</a>
+                    </li>
+                    <li>
+                        <a id="nav-item" href="/">Placeholder</a>
+                    </li>
+                </div>
             </div>
         </header>
     </body>
